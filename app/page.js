48d6f234d2e4d231033b4fbc17bc051f789b805b
AlexeyAdoniev <Alexeya@xp.network>
2023-11-14 02:04:29 +0200

    next setup

--- app/page.js
+++ app/page.js
@@ -1,95 +1,39 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import { Nav } from "@/common/nav";
+import { Montserrat } from "next/font/google";
+
+const montserrat = Montserrat({ subsets: ["cyrillic"], weight: "700" });
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="home">
+      <header>
+        <div className="top-line">
+          <Nav />
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+        <div className="container">
+          <div className="board">
+            <div className="board-head"></div>
+            <div className="board-content">
+              <div className="board-content_text">
+                <a href="">Github</a>
+                <h1 className={montserrat.className}>
+                  Alexey
+                  <br /> Adoniev
+                </h1>
+                <p>Passionate developer and web artist</p>
+              </div>
+              <div className="board-content_interactive">
+                <div className="search-bar"></div>
+                <div className="interactive-menu">
+                  <div className="interactive-menu_sidebar"></div>
+                  <div className="interactive-menu_image"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
     </main>
-  )
+  );
 }
---
+++ common/nav.jsx
@@ -0,0 +1,9 @@
+export const Nav = () => {
+    return <nav>
+        <ul>
+            <li><span>Home</span></li>
+            <li><span>About Me</span></li>
+
+        </ul>
+    </nav>
+}

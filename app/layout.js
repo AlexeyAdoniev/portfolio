03194034138c6dbcb9a44b69101e811ceed58b66
root <root@vps1tqxd.netxi.host>
2023-12-14 13:19:20 +0100

    viewport2

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,10 @@
 import { Montserrat } from "next/font/google";
 import "@/app/reset.css";
 import "@/app/animations.css";
-import "@/app/media.css";
 import "@/app/globals.css";
 import "@/public/fonts/fontawasome/css/fontawesome.min.css";
 import "@/public/fonts/fontawasome/css/solid.min.css";
+import "@/app/media.css";
 
 const montserrat = Montserrat({ subsets: ["latin"] });
 
@@ -17,10 +17,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <meta
-          name="viewport"
-          content="initial-scale=1, maximum-scale=1, user-scalable=yes"
-        />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <link rel="icon" href="/favicon.ico" sizes="any" />
       </head>
       <body className={montserrat.className}>{children}</body>
